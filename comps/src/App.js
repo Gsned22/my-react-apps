d7132e53ec8f6f41d5f2eb8d2595ad42e3cb2252
Garrett Snedeker <garrettsnedeker20@gmail.com>
2023-04-01 15:34:10 -0500

    before useReducer

--- comps/src/App.js
+++ comps/src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 
 
@@ -29,6 +30,9 @@ function App() {
           </Route>
           <Route path='/table'>
             <TablePage/>
+          </Route>
+          <Route path='/counter'>
+            <CounterPage initialCount={10}/>
           </Route>
         </div>
       </div>
